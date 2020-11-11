bfffc86b14f1bb6d341e787629cdead5ade60993
Aman Tank <AmanTank187@gmail.com>
2020-11-11 11:57:02 +0000

    Refactored methods

--- directory.rb
+++ directory.rb
@@ -1,21 +1,19 @@
-
+@students = [] # Array for all methods to use
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #Creating an array
-  students = []
+  @students = []
   #Get first name
   name = gets.chomp
   #While the name variable is not empty, next piece of code will run.
   while !name.empty? do
     #push the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #Get another name
     name = gets.chomp
   end
-  #return the students array
-  students
 end
 
 def print_header
@@ -23,39 +21,45 @@ def print_header
   puts "-----------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
   puts
 end
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
 
-def interactive_menu
-  students = []
-  loop do
-    #Showing a menu and asking the user for a input
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-  #Save the users input into a variable
-  selection = gets.chomp
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
 
+def process(selection)
   case selection
     when "1"
-      students = input_students
+      input_students
     when "2"
-      print_header
-      print(students)
-      print_footer(students)
+      show_students
     when "9"
       exit
     else
       puts "I don't know what you mean, try again"
     end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
