00d8287323da77d32eeec62f205e34c1a457e4c3
Aman Tank <AmanTank187@gmail.com>
2020-11-11 13:19:01 +0000

    Load feature added

--- directory.rb
+++ directory.rb
@@ -5,14 +5,14 @@ def input_students
   #Creating an array
   @students = []
   #Get first name
-  name = gets.chomp
+  name = STDIN.gets.chomp
   #While the name variable is not empty, next piece of code will run.
   while !name.empty? do
     #push the student hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     #Get another name
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -35,6 +35,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -52,6 +53,8 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit
     else
@@ -62,7 +65,7 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -76,6 +79,27 @@ def save_students
     file.puts csv_line
   end
   file.close
+end
+
+def load_students(filename = "students.csv")
+  file = File.open("students.csv","r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist."
+    exit
+  end
 end
 
 interactive_menu()
